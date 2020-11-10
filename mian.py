7841a13a62ef8109560fe1e575bc174faaa0361f
xinruoyusixian <xinruoyusixian@163.com>
2020-11-10 08:56:20 +0000

    Update mian.py

--- mian.py
+++ mian.py
@@ -9,12 +9,20 @@
 
 # main.py -- put your code herfr
 from LCD_12864G import LCD_12864G
-import time,framebuf,_lib,font
+import time,framebuf,_lib,font,ujson
 from machine import Pin 
+from urequests import get
 display=LCD_12864G()
-
 draw=display.draw
 
+
+def get_weather():
+  try:
+        data =  ujson.loads(get("http://www.tianqiapi.com/free/day?appid=75982964&appsecret=XtOL4MxG").text)
+        print("Weather Update")
+        return data
+  except:
+    print("geng xin shi bai !")
 def get_string(s):
       #在指定位置写入8*8的文字 会自动向后显示文字
       w=len(s)*8
@@ -30,8 +38,17 @@ def num(numbers,zimo,width=8,x=0,y=1):
   for a in range(0,length):
     if numbers[a]==':':
       index=10
+
+    elif numbers[a]=='-':
+      index=11
+
+    elif numbers[a]=='/':
+      index=12
+  
     else:
+      #print(numbers[a])
       index=int(numbers[a])
+    
     draw(zimo[index],width,x,y)
     x+=width
   
@@ -49,50 +66,40 @@ def timed_function(f, *args, **kwargs):
 
 
 def loop(t= 10):
+    i=1
     while 1:
-
-      Pin(15,1)
-      Pin(12,0)
+      if i==300:
+        _lib.update_time()
+        i=1
+      i+=1
       for a in range(0,100):
         _t=time.localtime()
         s=str("%02d" % _t[3])+str(":%02d" % _t[4])+str(":%02d" % _t[5])  
         time.sleep_ms(t)
+        draw(font.icon8x8['arrow'],8,a,2)
+        draw(font.xq[0],16,0,3)
+        draw(font.xq[1],16,16,3)
+        draw(font.wk[_t[6]],16,32,3)
+        num(str("%02d" % _t[0])+str("-%02d" % _t[1])+str(":%02d" % _t[2])  ,font.num8x8,8,50,3)
         num(s,font.num16x32,16,0,5) 
-        draw(font.hlwd[0],32,a)
-        draw(font.hlwd[1],32,a+32)
-        draw(font.hlwd[2],32,a+64)
-        draw(font.hlwd[3],32,a+64+32)
-      Pin(15,0)
-      Pin(12,1)
-      for a in range(0,100):
-        _t=time.localtime()
-        s=str("%02d" % _t[3])+str(":%02d" % _t[4])+str(":%02d" % _t[5])  
-        time.sleep_ms(t)
-        draw(font.hlwd[0],32,100-a)
-        draw(font.hlwd[1],32,100-a-32)
-        draw(font.hlwd[2],32,100-a-64)
-        draw(font.hlwd[3],32,100-a-64-32)
-        num(s,font.num16x32,16,0,5) 
-
-
 @timed_function
 def pic():
   display.pic(font.hlwd[0],32,0,32)
 
-
-#_lib.update_time()
 pic()
-
+_lib.update_time()
 loop()
-while 1:
-  
-  display.fill_rect(0,0,64,8,0)
-  time.sleep_ms(10)
-  _t=time.localtime()
-  s=str("%02d" % _t[3])+str(":%02d" % _t[4])+str(":%02d" % _t[5])
-  display.text(s)
-  display.fill_rect(10,10,82,8,0)
-  display.text(str(time.ticks_us()),10,10)
-  display.show()
 
-  
+
+def fbuf():
+  while 1:
+    
+    display.fill_rect(0,0,64,8,0)
+    time.sleep_ms(10)
+    draw(font.icon8x8['arrow'],8,0,3)
+    _t=time.localtime()
+    s=str("%02d" % _t[3])+str(":%02d" % _t[4])+str(":%02d" % _t[5])
+    display.text(s)
+    display.fill_rect(10,10,82,8,0)
+    display.text(str(time.ticks_us()),10,10)
+    display.show()
